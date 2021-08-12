735648348a308bb252372f2b0576d888b04777c3
Santiago Pegels <santiagopegels@gmail.com>
2021-08-12 18:47:00 -0300

    Add getl all user's queues method

--- controllers/queue.js
+++ controllers/queue.js
@@ -49,6 +49,7 @@ const addTicket = async (req, res) => {
 
 const getNextTicket = async (req, res) => {
     try {
+        const { screen } = req.body
         const queueId = req.params.id
 
         const queue = await Queue.findById(queueId)
@@ -60,15 +61,16 @@ const getNextTicket = async (req, res) => {
             })
         }
 
-        if (!queue.tickets) {
+        if (!queue.tickets.length > 0) {
             return res.status(400).json({
                 status: false,
-                message: 'No existe tickets disponibles.'
+                message: `No existe tickets disponibles para la fila ${queue.name}.`
             })
         }
 
         const ticket = queue.tickets.shift()
-        
+        ticket.screen = screen
+
         queue.ticketsAttended.push(ticket)
 
         await queue.save()
@@ -82,10 +84,34 @@ const getNextTicket = async (req, res) => {
     }
 }
 
+const getAllUserQueues = async (req, res) => {
+    try {
+
+        const userId  = req.uid
+        
+        const queues = await Queue.find({users: {$all: [userId]}})
+
+        if (!queues.length > 0) {
+            return res.status(400).json({
+                status: false,
+                message: 'No existen filas disponibles.'
+            })
+        }
+
+        return res.status(200).json({
+            status: true,
+            queues
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
 
 module.exports = {
     createQueue,
     addTicket,
-    getNextTicket
+    getNextTicket,
+    getAllUserQueues
 }
--- routes/queue.js
+++ routes/queue.js
@@ -2,14 +2,18 @@ const { Router } = require('express')
 const router = Router()
 const { check } = require('express-validator')
 const { validateFields } = require('../middlewares/validate-fields')
-const { createQueue, addTicket, getNextTicket } = require('../controllers/queue')
+const { 
+    createQueue, 
+    addTicket, 
+    getNextTicket, 
+    getAllUserQueues } = require('../controllers/queue')
 const { validateJWT } = require('../middlewares/validate-jwt')
 
 router.post('/',
     [
-        validateJWT,
         check('name', 'El nombre es obligatorio').notEmpty(),
         validateFields,
+        validateJWT,
     ],
     createQueue)
 
@@ -21,8 +25,16 @@ router.put('/:id/addTicket',
 
 router.get('/:id/nextTicket',
     [
+        check('screen', 'El puesto es requerido').notEmpty(),
+        validateFields,
         validateJWT,
     ],
     getNextTicket)
+
+router.get('/getAllUserQueues',
+    [
+        validateJWT,
+    ],
+    getAllUserQueues)
 
 module.exports = router;
